0a1a02a145f0fa657f5784d7dbe6158246d5b73e
kevin95all <58482652+kevin95all@users.noreply.github.com>
2021-09-23 22:01:14 -0600

    Actualizando

--- Pixel_Art/Automata.py
+++ Pixel_Art/Automata.py
@@ -1,4 +1,65 @@
 class Automata:
 
     def __init__(self):
-        pass
+        self.ruta = ''
+        self.contenido = ''
+        self.caracter = ''
+        self.token = ''
+        self.estado = 0
+        self.lista_nombres = []
+        self.lista_imagenes = []
+        self.imagen = []
+        self.lista_celdas = []
+        self.celda = []
+        self.filtros = []
+        self.lista_tokens = []
+        self.lista_errores = []
+
+    def letra(self, caracter):  # -----> Método para saber si el carácter es una letra
+        L = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
+             'Ñ', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+        if caracter in L:
+            return True
+        else:
+            return False
+
+    def digito(self, caracter):  # -----> Método para saber si el carácter es un digito
+        D = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+        if caracter in D:
+            return True
+        else:
+            return False
+
+    def leer_archivo(self, ruta):
+        self.ruta = ruta
+
+        with open(self.ruta, mode='r') as archivo:
+            self.contenido = archivo.read()
+
+        f = 1
+        c = 0
+        while c < len(self.contenido):
+            self.caracter = self.contenido[c]
+
+            if self.estado == 0:  # -----------------------------------> Estado 0
+                if self.letra(self.caracter):
+                    self.estado = 1
+                    self.lista_tokens.append(f'\tId\t\t Letras\t\t Fila:\t {f}\t\t Columna:\t {c+1}')
+                elif self.caracter == '@':
+                    self.estado = 34
+                    self.lista_tokens.append(f'\tSeparador\t\t Caracteres\t\t Fila:\t {f}\t\t Columna:\t {c+1}')
+                else:
+                    self.estado = 0
+                    self.lista_errores.append(f'\tFila:\t {f}\t\t Columna:\t {c+1}\t\t Error:\t {self.caracter}')
+            elif self.estado == 1:  # -----------------------------------> Estado 1
+                if self.letra(self.caracter):
+                    self.estado = 1
+                elif self.caracter == '=':
+                    self.estado = 2
+                else:
+                    self.estado = 1
+                    self.lista_errores.append(f'\tFila:\t {f}\t\t Columna:\t {c + 1}\t\t Error:\t {self.caracter}')
+            elif self.estado == 2:  # -----------------------------------> Estado 2
+                pass
+
+            c = c + 1
--- Pixel_Art/Main.py
+++ Pixel_Art/Main.py
@@ -1,4 +1,143 @@
+from tkinter import *
+from tkinter import filedialog
+from tkinter import messagebox
+from Automata import Automata
+import webbrowser
+import os
+
+
 class Main:
 
     def __init__(self):
-        pass
+        self.ventana = Tk()
+        self.imagen = Label(self.ventana)
+        self.automata = Automata()
+        self.archivo_analizado = False
+        self.ruta = ''
+
+    def ventana_principal(self):
+        self.ventana.title('Bitxelart')
+        self.centrar_ventana()
+        self.ventana.resizable(False, False)
+        self.componentes()
+        self.ventana.mainloop()
+
+    def centrar_ventana(self):
+        w, h = 850, 600
+        w_pantalla = self.ventana.winfo_screenwidth()
+        h_pantalla = self.ventana.winfo_screenheight()
+        x = ((w_pantalla / 2) - (w / 2))
+        y = ((h_pantalla / 2) - (h / 2)) - 50
+        self.ventana.geometry(f'{w}x{h}+{int(x)}+{int(y)}')
+
+    def componentes(self):
+        self.barra()
+        self.botones()
+
+    def barra(self):
+        menu = Menu(self.ventana)  # -----> Creando la barra de opciones
+
+        cargar = Menu(menu)  # -----> Creando las opciones para la barra
+        analizar = Menu(menu)
+        seleccionar = Menu(menu)
+        reportes = Menu(menu)
+        salir = Menu(menu)
+
+        cargar.add_command(label='1. Cargar archivo', command=self.cargar_archivo)  # -----> Creando las acciones
+        analizar.add_command(label='1. Analizar archivo', command=self.analizar_archivo)
+        seleccionar.add_command(label='1. Seleccionar imagen', command=self.seleccionar_imagen)
+        reportes.add_command(label='1. Ver reportes', command=self.ver_reportes)
+        salir.add_command(label='1. Salir', command=self.salir)
+
+        menu.add_cascade(label='Cargar', menu=cargar)  # -----> Agregando las opciones a la barra
+        menu.add_cascade(label='Analizar', menu=analizar)
+        menu.add_cascade(label='Seleccionar', menu=seleccionar)
+        menu.add_cascade(label='Reportes', menu=reportes)
+        menu.add_cascade(label='Salir', menu=salir)
+
+        self.ventana.config(menu=menu)  # -----> Agregando la barra de opciones al contenedor
+
+    def botones(self):
+        original = Button(self.ventana, text='Original', width=12, height=2, command=self.mostrar_o)
+        original.place(x=55, y=195)
+
+        mirrorx = Button(self.ventana, text='Mirror X', width=12, height=2, command=self.mostrar_x)
+        mirrorx.place(x=55, y=250)
+
+        mirrory = Button(self.ventana, text='Mirror Y', width=12, height=2, command=self.mostrar_y)
+        mirrory.place(x=55, y=305)
+
+        doublem = Button(self.ventana, text='Double Mirror', width=12, height=2, command=self.mostrar_d)
+        doublem.place(x=55, y=360)
+
+        self.imagen.configure(bg='black', width=85, height=33)
+        self.imagen.place(x=200, y=50)
+
+    def mostrar_o(self):
+        self.imagen.configure(bg='red')
+
+    def mostrar_x(self):
+        self.imagen.configure(bg='blue')
+
+    def mostrar_y(self):
+        self.imagen.configure(bg='green')
+
+    def mostrar_d(self):
+        self.imagen.configure(bg='cyan')
+
+    def cargar_archivo(self):  # -----> Método para la busqueda de archivos (pxla)
+        respaldo = self.ruta
+        self.ruta = ''
+
+        self.ruta = filedialog.askopenfilename(
+            title='Buscar archivo',
+            filetypes=[
+                ('Archivos PXLA', '*.pxla'),
+                ('Todos los archivos', '*.*')
+            ]
+        )
+        if self.ruta == '':
+            self.ruta = respaldo
+            messagebox.showinfo('Información', 'No se cargo ningun archivo')
+        else:
+            messagebox.showinfo('Información', 'Archivo cargado con exito')
+
+    def analizar_archivo(self):  # -----> Método para empezar con el analisis del archivo
+        if self.ruta == '':
+            messagebox.showinfo('Información', 'No hay archivos cargados')
+        else:
+            self.automata.leer_archivo(self.ruta)
+            self.archivo_analizado = True
+            messagebox.showinfo('Información', 'Archivo analizado con exito')
+
+    def seleccionar_imagen(self):
+        if self.ruta == '':
+            messagebox.showinfo('Información', 'No hay archivos cargados')
+        else:
+            if self.archivo_analizado:
+                pass
+            else:
+                messagebox.showinfo('Información', 'No se ha analizado el archivo')
+
+    def ver_reportes(self):
+        if self.ruta == '':
+            messagebox.showinfo('Información', 'No hay archivos cargados')
+        else:
+            if self.archivo_analizado:
+                chromedir = "C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s"
+                tokens = 'file:///' + os.getcwd() + '/' + 'archivos_creados/tokens.html'
+                errores = 'file:///' + os.getcwd() + '/' + 'archivos_creados/errores.html'
+
+                webbrowser.get(chromedir).open_new_tab(tokens)
+                webbrowser.get(chromedir).open_new_tab(errores)
+            else:
+                messagebox.showinfo('Información', 'No se ha analizado el archivo')
+
+    def salir(self):
+        self.ruta = ''
+        self.archivo_analizado = False
+        exit()
+
+
+app = Main()  # -----> app es un objeto de la clase Main
+app.ventana_principal()
